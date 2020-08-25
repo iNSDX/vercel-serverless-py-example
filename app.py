f968cc4346c1dc3fcda7746dd3a88d6149278548
iNSDX <lrp_ruspeg@hotmail.com>
2020-08-25 23:31:38 +0200

    get offer

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World-dev"}
+
+
+@app.get("/offer/{offer_id}")
+async def get_offer(offer_id: int):
+    return {"offer_id": offer_id}
